197afefe7bb66f16572ceefe2993caaa0f2fd9ff
OhBurro <46065883+OhBurro@users.noreply.github.com>
2020-05-22 00:57:41 +0100

    Update bot..js

--- bot..js
+++ bot..js
@@ -193,6 +193,7 @@ client.on("guildMemberAdd", async member => {
 
     client.on("guildMemberRemove", async member => {
         const out = member.guild.channels.find(channel => channel.name === 'programaçao-da-festa')
+        if(!channel) return;
         channel.send(`Xauu Xauuu ${member.user.username} foi para as reservas que se fodeu (${member.guild.name})`)
     })
 
