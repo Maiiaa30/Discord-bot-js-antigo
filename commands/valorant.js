1b409d36075e359afce03290d9a265b55feb934b
OhBurro <46065883+OhBurro@users.noreply.github.com>
2020-05-24 15:20:57 +0100

    Update valorant.js

--- commands/valorant.js
+++ commands/valorant.js
@@ -2,21 +2,30 @@ const Discord = require('discord.js');
 
 
 exports.run = (client, message, agrs) => {
-    if(args[1] === "maia") {
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setTitle('Valorant Maia Rank')
-        .setDescription("Silver 1")
+    const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTitle('Valorant Maiaa Rank')
+    .addField("Rank", "Silver 1" , true)
+    .addField("Name", "Likuyl#CDF" , true)
 
-        message.channel.send(embed)
-    }
+    message.channel.send(embed)
 
-    if(agrs[2] === "rui") {
-        const embed2 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setTitle('Valorant Rui Rank')
-        .setDescription("Gold 3")
+    const embed2 = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTitle('Valorant Rui Rank')
+    .addField("Rank", "Gold 3" , true)
+    .addField("Name", "IROKOMATA#BR1" , true)
+    .setFooter("Criado por Maia ❤️", client.user.avatarURL, true)
 
-        message.channel.send(embed2)
-    }
+    message.channel.send(embed2)
+
+    const embed3 = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTitle('Valorant Joao Rank')
+    .addField("Rank", "Gold 1" , true)
+    .addField("Name", "J1M1#Puta" , true)
+    .setFooter("Criado por Maia ❤️", client.user.avatarURL, true)
+
+    message.channel.send(embed3)
+
 }
